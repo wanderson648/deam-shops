7217caacdc05a4e618cb70f9404131ea462b6d56
wanderson648 <testepedroca@gmail.com>
2024-10-09 13:18:39 -0300

    configura e implementa model

---
+++ src/main/java/com/dailycodework/dreamshops/model/Category.java
@@ -0,0 +1,25 @@
+package com.dailycodework.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
