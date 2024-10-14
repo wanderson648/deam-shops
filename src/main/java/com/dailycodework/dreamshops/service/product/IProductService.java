743a4807dc4b16fa3fc350d6ec01298fc0b83a8f
wanderson648 <testepedroca@gmail.com>
2024-10-14 11:21:38 -0300

    implementa service de product

---
+++ src/main/java/com/dailycodework/dreamshops/service/product/IProductService.java
@@ -0,0 +1,20 @@
+package com.dailycodework.dreamshops.service.product;
+
+import com.dailycodework.dreamshops.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+
+    Product addProduct(Product product);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    void updateProduct(Product product, Long productId);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String category, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+}
